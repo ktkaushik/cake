fced7303f2a73a549ff5d8cd826ef08f3b69877f
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2014-10-16 18:40:55 +0530

    go to github

---
+++ Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $kaushik = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'cake',
+	);
+}
